1fbfd3189514d16644922f4938ad83cf8096e253
ShafSpecs <abbadshaf05@gmail.com>
2023-05-27 08:36:17 +0100

    Updating interfaces for service layer

--- services/interfaces.go
+++ services/interfaces.go
@@ -1,20 +1,10 @@
 package services
 
-import "scamazone-Go/data/models"
+import (
+	"scamazone-Go/dtos/requests"
+	"scamazone-Go/dtos/responses"
+)
 
-// CustomerService change this to normal funcs like login, etc.
 type CustomerService interface {
-	Create(customer *models.Customer) error
-	Update(customer models.Customer) error
-	Delete(id uint) error
-	FindById(id uint) (*models.Customer, error)
-	FindByEmail(email string) (*models.Customer, error)
-}
-
-type VendorService interface {
-	Create(vendor *models.Vendor) error
-	Update(vendor models.Vendor) error
-	Delete(id uint) error
-	FindById(id uint) (*models.Vendor, error)
-	FindByEmail(email string) (*models.Vendor, error)
+	CreateCustomer(request requests.CustomerRegisterRequest) (responses.CustomerRegisterResponse, error)
 }
