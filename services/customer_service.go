4b3e8017a551ee81a4e67e840ab27f589cef8d9c
ShafSpecs <abbadshaf05@gmail.com>
2023-05-27 08:35:48 +0100

    Still working on services

--- services/customer_service.go
+++ services/customer_service.go
@@ -1,7 +1,6 @@
 package services
 
 import (
-	"scamazone-Go/data/models"
 	"scamazone-Go/data/repositories"
 )
 
@@ -11,24 +10,4 @@ type CustomerServiceImpl struct {
 
 func NewCustomerService(customerRepo repositories.CustomerRepository) CustomerService {
 	return &CustomerServiceImpl{customerRepo}
-}
-
-func (service *CustomerServiceImpl) Create(customer *models.Customer) error {
-	return service.customerRepo.Create(customer)
-}
-
-func (service *CustomerServiceImpl) Update(customer models.Customer) error {
-	return service.customerRepo.Update(customer)
-}
-
-func (service *CustomerServiceImpl) Delete(id uint) error {
-	return service.customerRepo.Delete(id)
-}
-
-func (service *CustomerServiceImpl) FindById(id uint) (*models.Customer, error) {
-	return service.customerRepo.FindById(id)
-}
-
-func (service *CustomerServiceImpl) FindByEmail(email string) (*models.Customer, error) {
-	return service.customerRepo.FindByEmail(email)
 }
