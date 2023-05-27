3d525e6842d7d41fdb16499445e9bb1235dba5eb
ShafSpecs <abbadshaf05@gmail.com>
2023-05-27 08:35:35 +0100

    Still working on controllers

--- controllers/customer_controller.go
+++ controllers/customer_controller.go
@@ -10,17 +10,15 @@ import (
 
 func CustomerController(r *mux.Router, db *gorm.DB) {
 	customerRepo := repositories.NewCustomerRepository(db)
-	customerService := services.NewCustomerService(customerRepo)
+	_ = services.NewCustomerService(customerRepo)
 
 	customerRouter := r.PathPrefix("/api/v1/customers").Subrouter()
 
 	customerRouter.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
 		//_, _ = w.Write([]byte("Creating a customer"))
-		_ = customerService.Create(nil)
 	}).Methods("GET")
 
 	customerRouter.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
 		//_, _ = w.Write([]byte("Creating a customer"))
-		_ = customerService.Create(nil)
 	}).Methods("GET")
 }
